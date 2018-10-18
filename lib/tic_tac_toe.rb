dd9b315c9b4a02ffc70610536b38137e8cf2acff
Sheneeb Sadhik <sadhik.sheneeb@gmx.com>
2018-10-18 17:36:23 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,3 +57,14 @@ def turn(board)
     end
   end
 end
+
+def won?(board) WIN_COMBINATIONS.each do
+   |wincombo| 
+   if (board[wincombo[0]]) == "X" && (board[wincombo[1]]) == "X" && (board[wincombo[2]]) == "X" 
+     return wincombo 
+   elsif (board[wincombo[0]]) == "O" && (board[wincombo[1]]) == "O" && (board[wincombo[2]]) == "O" 
+     return wincombo 
+   end 
+ end 
+ false 
+end
