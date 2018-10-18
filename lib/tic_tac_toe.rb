e8af8027023883773dd6ab81a5cd0e88481b8254
Sheneeb Sadhik <sadhik.sheneeb@gmx.com>
2018-10-18 17:26:21 +0000

    automatically backed up by learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,59 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player)
+  board[index] = current_player
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+true  end
+end
+
+
+def valid_move?(board, index)
+  if index.between?(0,8) && position_taken?(board, index) == true
+    false
+  elsif index.between?(0,8) && position_taken?(board, index) == false
+    true
+  elsif !(index.between?(0,8)) && position_taken?(board, index) == false
+    false
+  end
+end
+
+def turn(board)
+  loop do
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(board, index) == false
+      move(board,index)
+      display_board(board)
+      break
+    end
+  end
+end
